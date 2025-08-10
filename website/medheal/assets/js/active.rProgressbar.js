ca7d9542b62cc8fcc3fbaccd839c4e93736e520b
youssefghamry <94710891+engyoussefghamry@users.noreply.github.com>
2025-08-10 20:46:02 +0300

    1
    1

---
+++ website/medheal/assets/js/active.rProgressbar.js
@@ -0,0 +1,16 @@
+(function($) {
+    'use strict';
+
+    $('.html').rProgressbar({
+        percentage: 100,
+        fillBackgroundColor: '#8ebf4c'
+    });
+    $('.css').rProgressbar({
+        percentage: 70,
+        fillBackgroundColor: '#8ebf4c'
+    });
+    $('.php').rProgressbar({
+        percentage: 90,
+        fillBackgroundColor: '#8ebf4c'
+    });
+})(jQuery);
